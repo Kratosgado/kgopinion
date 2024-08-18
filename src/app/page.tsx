60ba05db6a607c1a52252c4dad090ca9a87a7958
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-08-18 22:58:05 +0000

    start home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image";
+import styles from "./home.module.css";
+
 const Home = () => {
   return (
-    <div>
-      <h1>Home</h1>
+    <div className={styles.container}>
+      <div className={styles.textContainer}>
+        <h1 className={styles.title}>Kratosgado</h1>
+        <p className={styles.desc}>
+          This will be the description of the website. It will be a place where
+        </p>
+        <div className={styles.buttons}>
+          <button className={styles.button}>Learn More</button>
+          <button className={styles.button}>Contact</button>
+        </div>
+        <div className={styles.brands}>
+          <Image src="/brands.png" alt="" fill className={styles.brandImg} />
+        </div>
+      </div>
+      <div className={styles.imgContainer}>
+        <Image src="/hero.gif" alt="" fill className={styles.heroImg} />
+      </div>
     </div>
   );
 };
