6c61ebd067a4ed774133d3cea70d6ae30b2617b1
Prince Mbeah Essilfie <mbeahessilfieprince@gmail.com>
2024-08-18 21:01:12 +0000

    initial setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,9 @@
-import prisma from "@/lib/prisma";
-
-const getPosts = async () => {
-  const posts = await prisma.post.findMany();
-  return posts;
-};
-
-export default async function Page() {
-  const posts = await getPosts();
-
+const Home = () => {
   return (
     <div>
-      <h1>Blog Posts</h1>
-      <ul>
-        {posts.map((post) => (
-          <li key={post.id}>
-            <h2>{post.title}</h2>
-            <p>{post.content}</p>
-          </li>
-        ))}
-      </ul>
+      <h1>Home</h1>
     </div>
   );
-}
+};
+
+export default Home;
