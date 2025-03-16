045d9c55f708439802bf5518f034efb3d2ad0812
Kratosgado <mbeahessilfieprince@gmail.com>
2025-03-16 10:21:56 +0000

    contact page

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -1,5 +1,5 @@
 <script>
-	import { facebookLink, githubLink, twitterLink } from '$lib/utils';
+	import { facebookLink, githubLink, linkedInLink, twitterLink } from '$lib';
 </script>
 
 <!-- Footer -->
@@ -7,7 +7,7 @@
 	<div class="container mx-auto grid grid-cols-1 gap-8 md:grid-cols-3">
 		<div>
 			<span class="footer-title">KgOpinion</span>
-			<p class="mt-2">Providing reliable tech content since 2023</p>
+			<p class="mt-2">Providing reliable tech content since 2025</p>
 			<div class="mt-4 flex gap-4">
 				<a
 					href={twitterLink}
@@ -60,6 +60,11 @@
 						></path></svg
 					>
 				</a>
+         <a href={linkedInLink} aria-label="LinkedIn Profile"
+					target="_blank"
+          class="btn btn-circle btn-outline">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"></path><rect x="2" y="9" width="4" height="12"></rect><circle cx="4" cy="4" r="2"></circle></svg>
+          </a>
 			</div>
 		</div>
 		<div>
@@ -79,5 +84,5 @@
 	</div>
 </footer>
 <div class="footer footer-center bg-base-300 p-4 text-base-content">
-	<p>Copyright © 2023 - All rights reserved by KgOpinion</p>
+	<p>Copyright © 2025 - All rights reserved by KgOpinion</p>
 </div>
